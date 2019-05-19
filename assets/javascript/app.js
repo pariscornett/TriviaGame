b4539b07d502ea8260bdb7e057e3e2c1723e21a8
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-19 15:17:11 -0500

    styled theme and pseudocoded

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+//global  variables go here
+    //time left
+    //correct answers
+    //incorrect answers
+    //unanswered questions/timeout
+    //game complete yes or no
+
+    
+//make start button
+
+//make an object containing  var questionOne... var questionFive and a questionOneAnswers ["array"]
+
+//create a function to select a question and answer (put "ingredients" here to shuffle through the aforementioned object) MAKE SURE TO PUT ALL OPTIONS INSIDE THIS FUNCTION IN DESIRED ORDER OF APPEARANCE
+    //(VANILLA JS) call questionOne...Five
+
+    //(VANILLA JS) call corresponding questionOneAnswers...FiveAnswers
+
+    //(VANILLA JS) start a timer for 30 seconds to answer the question
+
+    //(JQUERY) display questionOne...Five in DOM
+
+    //(JQUERY) display corresponding questionOneAnswers...FiveAnswers in DOM 
+
+    //(JQUERY) display a timer that counts down the remaining time...try looking at %
+
+//create a function that handles scenarios of the timer runnning out and the user selecting an answer before the timer runs out (think about whether we use VANILLA JS or JQUERY)
+
+//create a function that decides if the answer is right or wrong (put "ingredients" here to go between question and answer pairs)---
+    //if right
+        //(VANILLA JS) do something
+        //(JQUERY) display something in the DOM
+    //else this
+        //(VANILLA JS) do something
+        //(JQUERY) display something in the DOM
+
+//create an *all done* page to summarize the game
+    //include a start over function that doesn't reload the game
+
+
+//CALL THE FUNCTIONS TO PLAY:----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
