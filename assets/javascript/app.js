07e2bb1c7baf15cd79a4f7d7b5bb674d4e21df33
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-20 22:08:56 -0500

    made start button show question and answer boxes upon click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,25 +7,32 @@ var gameComplete = false;
 
 //hide Q&A boxes, show box used for start button 
 $(".box2, .box3, .box4, .questionBox").hide();
-$(".box1").html("Let's Do This");
+$(".start").html("Let's Do This");
 
+//make start button clickable
+document.getElementById("start")
+$(".start").click(function(){
+    $(" .box1, .box2, .box3, .box4, .questionBox").show();
+    $(".start").empty();
+})
 
 //make an object containing  var questionOne... var questionFive and a questionOneAnswers ["array"]
 var questions = [
     {
         question: "Marsha Rhea Williams was the first black woman in the United States to earn a Ph.D in Computer Science. Where did she earn her degree?",
-        options: ["MIT", "Stanford", "Vanderbilt", "Howard"],
-        answer: this.options[2],
+        options:["MIT", "Stanford", "Vanderbilt", "Howard"],
+   
     },
+    
     {
         question: "Who is considered the first computer programmer?",
         options: ["Katherine Johnson", "Ada Lovelace", "Radia Perlman", "Dorothy Vaughan"],
-        answer: this.options[1],
+        //answer: this.options[1],
     },
     {
         question: "Rear Admiral Grace Hopper is lauded for this contribution to computing...",
         options: ["FORTRAN", "compilers", "COBOL", "all of the above"],
-        answer: this.options[3],
+        //answer: this.options[3],
     },
 ]
 
