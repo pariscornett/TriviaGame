16c36bb1e45a3f891774883c0052ad85e2948b3a
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-20 22:30:22 -0500

    changed positioning of questionBox from fixed to absolute so that it did not move with the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,11 @@ $(".start").html("Let's Do This");
 //make start button clickable
 document.getElementById("start")
 $(".start").click(function(){
+    //show the other boxes we need for remainder of game
     $(" .box1, .box2, .box3, .box4, .questionBox").show();
+    //empty the preexisting text in box1 which shares the start class
     $(".start").empty();
+    //run function that shows the question and answers text in the DOM 
     loadQuestions();
 })
 
@@ -23,39 +26,39 @@ var questions = [
     {
         question: "Marsha Rhea Williams was the first black woman in the United States to earn a Ph.D in Computer Science. Where did she earn her degree?",
         options:["MIT", "Stanford", "Vanderbilt", "Howard"],
-   
     },
     
     {
         question: "Who is considered the first computer programmer?",
-        options: ["Katherine Johnson", "Ada Lovelace", "Radia Perlman", "Dorothy Vaughan"],
-        //answer: this.options[1],
+        options: ["Katherine Johnson", "Ada Lovelace", "Radia Perlman", "Dorothy Vaughan"], 
     },
     {
         question: "Rear Admiral Grace Hopper is lauded for this contribution to computing...",
         options: ["FORTRAN", "compilers", "COBOL", "all of the above"],
-        //answer: this.options[3],
     },
 ]
 
 
 //create a function to select a question and answer (put "ingredients" here to shuffle through the aforementioned object) MAKE SURE TO PUT ALL OPTIONS INSIDE THIS FUNCTION IN DESIRED ORDER OF APPEARANCE
 function loadQuestions(){
+    //create variable for the question
     var questionOne = questions[0].question;
+    //create variable for the list of possible answers
     var questionOneAnswers = questions[0].options;
+    //create variable for the correct answer
     var correctAnswer = questionOneAnswers[2];
+    //insert all of that information into the DOM 
     $(".questionBox").html(questionOne);
     $(".box1").html(questionOneAnswers[0]);
     $(".box2").html(questionOneAnswers[1]);
     $(".box3").html(questionOneAnswers[2]);
     $(".box4").html(questionOneAnswers[3]);
     console.log(correctAnswer);
+    $(".box1, .box2, .box3, .box4").click(function(){
+        alert("you clicked me!");
+    })
 }
 
-//make start button
-// var startButton = document.createElement("button");
-// button.innerHTML = "Let's Do This";
-//append start button
 
 
 
