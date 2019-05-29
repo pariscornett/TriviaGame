9a93b32e6d8bb61dbc54a8b409c9fc14d4beefda
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-28 19:24:11 -0500

    added in space for timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,55 @@
+$(document).ready(function(){
 //global  variables go here
 var timeLeft
-var correctAnswers
-var incorrectAnswers
-var timeoutQuestions
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var timedOut = 0;
 var gameComplete = false;
+var timer = 30;
+//make an object containing  var questionOne... var questionFive and a questionOneAnswers ["array"]
+var questions = [
+    {
+        question: "Marsha Rhea Williams was the first black woman in the United States to earn a Ph.D in Computer Science. Where did she earn her degree?",
+        options: {
+            a:"MIT",
+            b: "Stanford", 
+            c:"Vanderbilt", 
+            d: "Howard"},
+    },
+    {
+        question: "Who is considered the first computer programmer?",
+        options: ["Katherine Johnson", "Ada Lovelace", "Radia Perlman", "Dorothy Vaughan"], 
+    },
+    {
+        question: "Rear Admiral Grace Hopper is lauded for this contribution to computing...",
+        options: ["FORTRAN", "compilers", "COBOL", "all of the above"],
+    },
+]
 
 //hide Q&A boxes, show box used for start button 
 $(".box2, .box3, .box4, .questionBox").hide();
 $(".start").html("Let's Do This");
+
+//create a function to select a question and answer (put "ingredients" here to shuffle through the aforementioned object) MAKE SURE TO PUT ALL OPTIONS INSIDE THIS FUNCTION IN DESIRED ORDER OF APPEARANCE
+function loadQuestions(){  
+    //set timer to 30 seconds
+    var timer = setInterval(progress(),1000 * 30);
+    //showe timer in DOM 
+    $(".timer").html("Time Left: " + timer);
+    //create variable for the question
+    var questionOne = questions[0].question;
+    //create variable for the list of possible answers
+    var questionOneAnswers = questions[0].options;
+    //create variable for the correct answer
+    var correctAnswer = questions[0].options.c;
+    console.log(correctAnswer);
+    //insert all of that information into the DOM 
+    $(".questionBox").html(questionOne);
+    $(".box1").html(questionOneAnswers.a);
+    $(".box2").html(questionOneAnswers.b);
+    $(".box3").html(questionOneAnswers.c);
+    $(".box4").html(questionOneAnswers.d);
+}
 
 //make start button clickable
 document.getElementById("start")
@@ -20,44 +62,28 @@ $(".start").click(function(){
     loadQuestions();
 })
 
-
-//make an object containing  var questionOne... var questionFive and a questionOneAnswers ["array"]
-var questions = [
-    {
-        question: "Marsha Rhea Williams was the first black woman in the United States to earn a Ph.D in Computer Science. Where did she earn her degree?",
-        options:["MIT", "Stanford", "Vanderbilt", "Howard"],
-    },
-    
-    {
-        question: "Who is considered the first computer programmer?",
-        options: ["Katherine Johnson", "Ada Lovelace", "Radia Perlman", "Dorothy Vaughan"], 
-    },
-    {
-        question: "Rear Admiral Grace Hopper is lauded for this contribution to computing...",
-        options: ["FORTRAN", "compilers", "COBOL", "all of the above"],
-    },
-]
-
-
-//create a function to select a question and answer (put "ingredients" here to shuffle through the aforementioned object) MAKE SURE TO PUT ALL OPTIONS INSIDE THIS FUNCTION IN DESIRED ORDER OF APPEARANCE
-function loadQuestions(){
-    //create variable for the question
-    var questionOne = questions[0].question;
-    //create variable for the list of possible answers
-    var questionOneAnswers = questions[0].options;
-    //create variable for the correct answer
-    var correctAnswer = questionOneAnswers[2];
-    //insert all of that information into the DOM 
-    $(".questionBox").html(questionOne);
-    $(".box1").html(questionOneAnswers[0]);
-    $(".box2").html(questionOneAnswers[1]);
-    $(".box3").html(questionOneAnswers[2]);
-    $(".box4").html(questionOneAnswers[3]);
-    console.log(correctAnswer);
-    $(".box1, .box2, .box3, .box4").click(function(){
-        alert("you clicked me!");
-    })
+//make a function that shows how well the player is doing after each question 
+function progress(){
+    //empty existing boxes
+    $(".questionBox, .box1, .box2, .box3, .box4").empty();
+    //show correct, incorrect and timed out answers in DOM
+    $(".correct").html ("You've answered " + correctAnswers + "correctly");
+    $(".incorrect").html ("You've answered " + incorrectAnswers + "incorrectly");
+    $(".time-out").html ("You've let " + timedOut + " questions time out");  
 }
+
+//call to the previous function when timer has run out--use clearTimeout when the user clicks an answer to go ahead and trigger the function and show progress
+function grade () {
+    if (timer == 0){
+        timedOut++;
+        progress();
+    }
+}
+
+
+
+
+
 
 
 
@@ -76,8 +102,10 @@ function loadQuestions(){
 //create an *all done* page to summarize the game
     //include a start over function that doesn't reload the game
 
+//end
+});
 
-//CALL THE FUNCTIONS TO PLAY:----------------------------------------------------------
+
 
 
 
